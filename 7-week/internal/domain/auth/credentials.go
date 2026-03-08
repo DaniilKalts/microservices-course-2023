7187dfc3b644cfdbd79cc7d00f7f10444f8d7f4a
DaniilKalts <kaltsdaniil@gmail.com>
2026-03-08 12:59:02 +0500

    refactor(7-week): remove non-domain entities

--- 7-week/internal/domain/auth/credentials.go
+++
@@ -1,9 +0,0 @@
-package auth
-
-import domainUser "github.com/DaniilKalts/microservices-course-2023/7-week/internal/domain/user"
-
-type Credentials struct {
-	ID           string
-	PasswordHash string
-	Role         domainUser.Role
-}
